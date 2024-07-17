b0d985966728d6898b368baef295f810914bc25b
Murray Sinclair <msin981@gmail.com>
2024-07-18 00:32:11 +1200

    chore(App): firstpass testing api to server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import './App.css'
+import { fetchDate } from './apis/dates'
 
 function App() {
+  const [date, setDate] = React.useState()
+
+  async function getDate() {
+    await fetchDate().then((res: any) => {
+      setDate(res.data.time)
+    })
+  }
+
+  React.useEffect(() => {
+    getDate()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <div id="date-box">
+          <h4>Date:</h4>
+          <p>{date}</p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
