de44b2c1f56fd7d55514296d913fbb5945eb1b04
Murray Sinclair <msin981@gmail.com>
2024-08-05 15:01:03 +1200

    feat(settings): INSTALLED_APPS tracks rest_framework and weather

--- settings.py
+++ settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'corsheaders',
+    'weather'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'urls'
-
+ 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
