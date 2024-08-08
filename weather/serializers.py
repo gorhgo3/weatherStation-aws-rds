fcc7096890cf5a00d593271f83e5709390ef1559
Murray Sinclair <msin981@gmail.com>
2024-08-09 01:33:26 +1200

    feat(serializers): firstpass additional serializer classes

--- weather/serializers.py
+++ weather/serializers.py
@@ -12,4 +12,17 @@ class WeatherRecordSerializer(serializers.ModelSerializer):
 class HumiditySerializer(serializers.ModelSerializer):
     class Meta:
         model = HumidityRecord
+class TemperatureSerializer(serializers.ModelField):
+    class Meta: 
+        # model = 
+        fields = "__all__"
+
+class WindSpeedSerializer(serializers.ModelField):
+    class Meta: 
+        # model = 
+        fields = "__all__"
+        
+class LuxSerializer(serializers.ModelField):
+    class Meta: 
+        # model = 
         fields = "__all__"
