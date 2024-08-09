ddad2ef68cf64c436474013dca91d2dd049ee5a9
Murray Sinclair <msin981@gmail.com>
2024-08-09 12:37:26 +1200

    refactor(weather): urls.py update routing

--- weather/urls.py
+++ weather/urls.py
@@ -1,9 +1,10 @@
 # weather/urls.py
 
 from django.urls import path
-from .views import WeatherRecordListCreate, WeatherRecordDetail, HumidityGeneric, HumidityDetail
+from .views import WeatherRecordListCreate, WeatherRecordDetail, HumidityGeneric, HumidityDetail, WeatherStationApi
 
 urlpatterns = [
-    path('api/humidity/', HumidityGeneric.as_view(), name='humidity-list'),
-    path('api/humidity/<int:pk>/', HumidityDetail.as_view(), name='humidity-list'),
+    path('api/add/weather/', WeatherStationApi.as_view(), name='humidity-list'),
+    path('api/humidity/<int:pk>/', HumidityDetail.as_view()),
+    path('api/humidity/', HumidityGeneric.as_view()),
 ]
