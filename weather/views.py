088479161b16a7dd638990f0e805501c188b2ea3
Murray Sinclair <msin981@gmail.com>
2024-08-09 12:52:16 +1200

    feat(views): WeatherStationApi() handles new data

--- weather/views.py
+++ weather/views.py
@@ -3,6 +3,9 @@
 from rest_framework import generics
 from .models import WeatherRecord, HumidityRecord
 from .serializers import WeatherRecordSerializer, HumiditySerializer
+from rest_framework import generics, status
+from rest_framework.response import Response
+
 
 class WeatherRecordListCreate(generics.ListCreateAPIView):
     queryset = WeatherRecord.objects.all()
@@ -22,3 +25,37 @@ class HumidityDetail(generics.RetrieveAPIView):
     queryset = HumidityRecord.objects.all()
     serializer_class = HumiditySerializer
     lookup_field = 'pk'
+
+
+class WeatherStationApi(generics.CreateAPIView):
+    def post(self, request, *args, **kwargs):
+      reqData = request.data
+      serializerErrors = []
+
+      if 'humidity' in reqData:
+          serializer = HumiditySerializer(data={'humidity':reqData['humidity']})
+          if serializer.is_valid():
+              serializer.save()
+          else:
+              serializerErrors.append(serializer.errors)
+
+      """
+      if 'temperature' in reqData:
+          serializer = TemperatureSerializer(data={'temperature':reqData['temperature']})
+          if serializer.is_valid():
+              serializer.save()
+          else:
+              serializerErrors.append(serializer.errors)
+      
+      if 'lux' in reqData:
+          serializer = LuxSerializer(data={'lux':reqData['lux']})
+          if serializer.is_valid():
+              serializer.save()
+          else:
+              serializerErrors.append(serializer.errors)
+      """
+      
+      if len(serializerErrors) != 0:
+        return Response(serializerErrors, status=status.HTTP_400_BAD_REQUEST)
+      else:         
+        return Response({'status': 'success'}, status=status.HTTP_200_OK)
